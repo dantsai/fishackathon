444540a7fc870cf2bbbce0f15f6bee636994fc01
krushton <kjrushton@gmail.com>
2014-06-14 21:16:16 -0700

    updated more stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require jquery.dd.min
+//= require jquery_ujs
 //= require jquery.dataTables
 //= require turbolinks
 //= require_tree .
@@ -33,4 +33,44 @@ $(document).ready(function() {
  
         e.preventDefault();
     });
-} );
+
+    $('#lookuploc').click(function() {
+        setHTML5Location();
+    });
+
+});
+
+
+function setHTML5Location() {
+    if (navigator.geolocation) {
+
+        var html5TimeStamp = null;
+        var html5Accuracy = null;
+
+        navigator.geolocation.getCurrentPosition(function (position, html5Error) {
+            processGeolocationResult(position);
+        },error,
+        {
+         timeout: 5000,
+         enableHighAccuracy: true,
+         maximumAge: Infinity
+         });
+    }
+    else {
+        alert("Sorry, your browser does not support geolocation");
+    }
+}
+
+function processGeolocationResult(position) {
+    html5Lat = position.coords.latitude; //Get latitude
+    html5Lon = position.coords.longitude; //Get longitude
+    html5TimeStamp = position.timestamp; //Get timestamp
+    html5Accuracy = position.coords.accuracy;   //Get accuracy in meters
+
+    console.log("success " + html5Lat + ", " + html5Lon);
+    $('#latlong').text(html5Lat.toString().substr(0,7) + ", " + html5Lon.toString().substr(0,7)).css('display','inline')
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
